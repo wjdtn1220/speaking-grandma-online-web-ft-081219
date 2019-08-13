ae1634c4d5488c113346c6f44f002d0b612f078c
Jeong Park <wjdtn1220@gmail.com>
2019-08-13 03:00:17 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -3,8 +3,10 @@
 def speak_to_grandma(phrase)
   if phrase == phrase.upcase
     return "NO, NOT SINCE 1938!"
-  else
+  elsif phrase != phrase.upcase
     return "HUH?! SPEAK UP, SONNY!"
+  else
+    return "I LOVE YOU TOO PUMPKIN!"
   end
 end
 # Whatever you say to grandma, she should respond with
